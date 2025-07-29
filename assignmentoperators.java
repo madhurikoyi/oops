79f1221bf5887b849d10ac58223f222ff2186d6d
madhurikoyi <koyimadhuri09@gmail.com>
2025-07-29 14:33:15 +0530

    assignmentoperator

---
+++ assignmentoperators.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class assignmentoperators {
+
+	public static void main(String[] args) {
+		int a=4,b=1;
+	    System.out.println(a);//4
+	    System.out.println(b);//1
+	    a = a * b;
+	        b = a / b;
+	    a = a / b;
+	    System.out.println(a);//1
+	    System.out.println(b);//4
+		// TODO Auto-generated method stub
+
+	}
+
+}
