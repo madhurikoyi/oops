4c5df6eb8d462bc6d418b76a7ac581f00881fed1
madhurikoyi <koyimadhuri09@gmail.com>
2025-07-29 14:26:08 +0530

    operators

---
+++ operators.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class operators {
+
+	public static void main(String[] args) {
+		System.out.println(5<2); //false
+        System.out.println(5<=2); //false
+   System.out.println(5>2); //true
+   System.out.println(5>=2); //true
+    System.out.println(5==2); //false
+   System.out.println(5!=2); //true
+		// TODO Auto-generated method stub
+
+	}
+
+}
