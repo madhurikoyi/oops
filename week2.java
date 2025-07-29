dfe838737106cbc63d0948bde7dafbecc314a629
madhurikoyi <koyimadhuri09@gmail.com>
2025-07-29 13:54:21 +0530

    week2

---
+++ week2.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class week2 {
+
+	public static void main(String[] args) {
+		System.out.println(5+2); //7
+		System.out.println(5-2); //3
+		System.out.println(5*2); //10
+		System.out.println(5/2); //2
+		System.out.println(5%2); //1
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
