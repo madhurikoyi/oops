4e6daf3b5f3a6c1e12793fa2e2b4c9716e2e7be4
madhurikoyi <koyimadhuri09@gmail.com>
2025-08-02 11:19:18 +0530

    string

---
+++ stings.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class stings {
+
+	public static void main(String[] args) {
+		System.out.print("Java\n"); // System.out.println("Java"); 
+		System.out.print("Python\n");
+		System.out.print("LINUX/UNIX");		
+		// TODO Auto-generated method stub
+
+	}
+
+}
