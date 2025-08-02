f799b8a7a2b460439bbe76037a1a5f140fe05daf
madhurikoyi <koyimadhuri09@gmail.com>
2025-08-02 10:57:04 +0530

    simpleif

---
+++ simpleif.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class simpleif {
+
+	public static void main(String[] args) {
+		int PI=3,radius=3;
+		int area;
+		area = radius * radius * PI;
+		  System.out.println("The area for the circle of radius " +radius + " is " + area);
+		// TODO Auto-generated method stub
+
+	}
+
+}
