32ae91c8d8c831e0dec9b0be3f53ad2f367f1be0
madhurikoyi <koyimadhuri09@gmail.com>
2025-08-02 10:43:22 +0530

    ternary

---
+++ Ternary.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class Ternary {
+
+	public static void main(String[] args) {
+		
+		    int x=41;
+		    System.out.println((x%2==0)?"even number":"odd number");//
+		  }
+		}
+		
+
+
